4d70285c223120569b2d87c67ad41be4ec415a3d
git2004 <48566560+git2004@users.noreply.github.com>
2024-09-30 13:58:14 +0300

    Add files via upload

---
+++ lr1_igor_2309.asm
@@ -0,0 +1,151 @@
+.model small
+.186
+.stack 100h
+.data
+A db 8fh
+B db 10h
+C db 127d
+D dw ?
+message1 dw "=A", 0dh, 0ah
+message2 dw "=B", 0dh, 0ah
+message3 dw "=C", 0dh, 0ah
+message4 db "-", 0dh, 0ah
+filename db "perepoln.txt", 0
+handle	dw ?
+buffer	db 4096 dup(?)
+.code
+start:
+	mov ax, @data
+	mov ds, ax
+	mov es, ax
+
+	xor dx,dx 
+	mov al, C
+	imul al 
+	shl ax, 2 
+	mov bx, ax 
+	shl ax, 1 
+	add ax, bx 
+	adc dx , 0h
+	mov bl, A  
+	xchg ax,bx 
+	cbw  
+	add ax, bx 
+	jz ZZ 
+	cmp dh, dl
+	jne perepoln
+	mov si, ax
+	imul al
+	mov bl, C
+	xchg ax,bx
+	cbw 
+	sub bx,ax
+	mov al, B
+	imul al
+	mov cx,ax 
+	shl ax, 2 
+	mov di, ax
+	shl ax, 7 
+	add ax, cx 
+	add ax, di 
+	add ax, bx 
+	idiv si
+	mov [D], ax
+	xor si,si
+	jmp Z
+	
+	jmp perepoln
+
+ZZ:
+	jmp Z
+
+perepoln:
+
+;create file	
+	mov	ah, 3ch
+	mov	dx, offset filename
+	mov	cx, 0h
+	int 21h
+	mov	handle, ax
+	
+	lea di, buffer
+	mov	bx, 10h
+
+A_output:
+	mov ax, message1
+	stosw
+	xor	ax, ax
+	mov 	al, A
+	xor	cx, cx
+	call 	to_ascii
+	inc	di
+
+B_output:
+	mov ax, message2
+	stosw
+	xor	ax, ax
+	mov 	al, B
+	mov	bx, 10h
+	xor	cx, cx
+	call 	to_ascii
+	inc	di
+
+C_output:
+	mov ax, message3
+	stosw
+	xor	ax, ax
+	mov 	al, C
+	mov	bx, 10h
+	xor	cx, cx
+	call 	to_ascii
+	mov	al, 0dh
+	mov	[di], al
+	inc	di
+;test
+	mov ax, message3
+	stosw
+
+;write file
+	mov	bx, handle
+	mov	ah, 40h
+	mov	cx, 200h
+	mov	dx, offset buffer
+	int 21h
+
+;close file
+	mov	bx, handle
+	mov	ah, 3eh
+	int 21h
+	mov	al, '$'
+	inc	di
+	mov	[di], al
+	mov	ah, 09h
+	int 21h
+
+Z:
+mov ah, 4Ch
+int 21h
+
+to_ascii:
+	xor	dx, dx
+	div	bx 
+st1:
+	inc	cx
+	cmp	ax, 9h
+	jng	add30h
+	add	ax, 37h
+	jmp	nxt
+add30h:
+	add	al, 30h
+nxt:
+	stosb
+	xchg	ax, dx
+	cmp 	cx, 1h
+	je	st1
+	mov  	al, "h"
+	mov 	[di], al
+	inc 	di
+ret
+
+end start
+
