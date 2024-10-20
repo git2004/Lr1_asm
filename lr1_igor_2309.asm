b72b720a6ce9033b3225735950819284ac677c48
git2004 <48566560+git2004@users.noreply.github.com>
2024-10-20 22:36:06 +0300

    Update lr1_igor_2309.asm

--- lr1_igor_2309.asm
+++ lr1_igor_2309.asm
@@ -2,31 +2,47 @@
 .186
 .stack 100h
 .data
-A db 126
-B db -110
-C db -14
+A db 1 dup(?)
+B db 1 dup(?)
+C db 1 dup(?) ; все что выше 73 переполнение 74+ 4Ah
 D dw ?
-message1 dw "=A", 0dh, 0ah
+message1 dw "=A", 0dh
 message2 dw "=B", 0dh, 0ah
 message3 dw "=C", 0dh, 0ah
 message4 db "-", 0dh, 0ah
 filename db "perepoln.txt", 0
 handle	dw ?
-buffer	db 4096 dup(?)
+buffer	db 8192 dup(?)
 .code
 start:
 	mov ax, @data
 	mov ds, ax
 	mov es, ax
 
+	mov al, A
+	or al, al
+	jnz program_main2
+	mov al, B
+	or al, al
+	jnz program_main2
+	mov al, C
+	or al, al
+	jnz program_main2
+	
+	mov bp, 1h
+	jmp checking
+
+program_main2:
+	mov bp, 1000h
+checking:
 	xor dx,dx 
-	mov al, C ;c
-	imul al  ;c2
+	mov al, C
+	imul al 
 	shl ax, 2 
 	mov bx, ax 
 	shl ax, 1 
 	add ax, bx 
-	adc dx , 0h ;для чего это нужно?
+	adc dx , 0h
 	mov bl, A  
 	xchg ax,bx 
 	cbw  
@@ -34,48 +50,99 @@ start:
 	jz ZZ 
 	cmp dh, dl
 	jne perepoln
+
 	mov si, ax
-	mov al, a ; дописано
-	imul al ; непонятно
+	mov al, A
+	imul al
 	mov bl, C
 	xchg ax,bx
 	cbw 
 	sub bx,ax
+	xor ax, ax
+	xchg bx, di
+
 	mov al, B
-	imul al
-	mov cx,ax 
-	CWD
-	shl ax, 2 
-	mov di, ax
-	CWD
-	shl ax, 7 ; переполнение
-	add ax, cx 
-	CWD
-	add ax, di 
-	add ax, bx 
-	idiv si
+	cbw
+	mov cx, ax; cx b
+	shl ax, 2 ; 4b
+	mov bx, ax ; bx 4b
+	shl ax, 5; 128b
+	add ax, cx; 129b
+	xchg ax,cx
+	imul ax
+	xchg ax,cx
+	imul bx; 516b^2
+	add ax, cx; 517b^2
+	add ax, di
+	mov bx, si
+	idiv bx
 	mov [D], ax
 	xor si,si
-	jmp Z
-	
-	jmp perepoln
-
 ZZ:
+	cmp bp, 1000h
+	jne our_circle
 	jmp Z
 
 perepoln:
+	cmp bp, 1h
+	je creating
+	cmp bp, 1000h
+	je creating
 
-;create file	
+	jmp next_loop
+
+creating:
+	dec bp
+	call create_file
+	
+next_loop:
+	mov bx, 10h
+	call a_output
+	call b_output
+	call c_output
+	call write_file
+	lea di, buffer
+	cmp bp, 0FFFh
+	je Z_end
+				
+our_circle: 	
+
+next_a:
+	cmp [A], 0FFh
+	je next_b
+	inc [A]
+	;inc [A]
+	jne next_circle
+next_b:
+	mov [A], 0h
+	cmp [B], 0FFh
+	je next_c
+	inc [B]
+	jne next_circle
+next_c:
+	mov [B], 0h
+	cmp [C], 0FFh
+	je Z_end
+	inc [C]
+
+next_circle:	
+	jmp checking
+Z_end:
+	call close_file
+Z:
+mov ah, 4Ch
+int 21h
+
+create_file:	
 	mov	ah, 3ch
 	mov	dx, offset filename
 	mov	cx, 0h
 	int 21h
 	mov	handle, ax
-	
 	lea di, buffer
-	mov	bx, 10h
+ret
 
-A_output:
+a_output:
 	mov ax, message1
 	stosw
 	xor	ax, ax
@@ -83,8 +150,9 @@ A_output:
 	xor	cx, cx
 	call 	to_ascii
 	inc	di
+ret
 
-B_output:
+b_output:
 	mov ax, message2
 	stosw
 	xor	ax, ax
@@ -93,8 +161,9 @@ B_output:
 	xor	cx, cx
 	call 	to_ascii
 	inc	di
+ret
 
-C_output:
+c_output:
 	mov ax, message3
 	stosw
 	xor	ax, ax
@@ -102,33 +171,29 @@ C_output:
 	mov	bx, 10h
 	xor	cx, cx
 	call 	to_ascii
-	mov	al, 0dh
+	mov	al, 0ah
 	mov	[di], al
-	inc	di
-;test
-	mov ax, message3
-	stosw
+	;inc	di
+ret
 
-;write file
+write_file:
 	mov	bx, handle
 	mov	ah, 40h
-	mov	cx, 200h
+	mov	cx, 14h
 	mov	dx, offset buffer
 	int 21h
+ret
 
-;close file
-	mov	bx, handle
-	mov	ah, 3eh
-	int 21h
+close_file:
+	;mov	bx, handle
+	;mov	ah, 3eh
+	;int 21h
 	mov	al, '$'
 	inc	di
 	mov	[di], al
-	mov	ah, 09h
-	int 21h
-
-Z:
-mov ah, 4Ch
-int 21h
+	;mov	ah, 09h
+	;int 21h
+ret
 
 to_ascii:
 	xor	dx, dx
